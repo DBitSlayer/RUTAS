51f84a3af6cde67386f0ba0efa8acba624770157
DBitSlayer <sanchez_3099@hotmail.com>
2021-04-02 13:52:54 -0500

    App Base

--- app/src/main/java/com/py/rutas/MainActivity.java
+++ app/src/main/java/com/py/rutas/MainActivity.java
@@ -2,13 +2,32 @@ package com.py.rutas;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Button btnSitios;
+    private Button btnTipos;
+    private Button btnUbicacion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        btnSitios = findViewById(R.id.btn_sitios);
+        btnTipos = findViewById(R.id.btn_Tipos);
+        btnUbicacion = findViewById(R.id.btn_Ubicacion);
+
+        btnSitios.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(),Mapa.class);
+                startActivity(intent);
+            }
+        });
     }
 }
